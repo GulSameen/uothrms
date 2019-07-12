a2339e74bc986bb139aa67f8986c53761a483173
gulsameen <gulsameen.se17@iba-suk.edu.pk>
2019-07-12 14:22:27 +0500

    initial commit

---
+++ src/main/java/uot/uothrms/UothrmsApplication.java
@@ -0,0 +1,13 @@
+package uot.uothrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UothrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UothrmsApplication.class, args);
+    }
+
+}
